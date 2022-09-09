4b5b994d3146f6d3843f3c389488a9554ed1940d
praisewairimu <misssampraise@gmail.com>
2022-09-09 09:22:15 -0700

    My first line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#!/bin/bash
+##!/bin/bash
 #include <stdio.h>
 
 /**
- * main -uts("\"Programming is like building a multilingual puzzle"):)) )  ) )
- return (0);
- }
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
