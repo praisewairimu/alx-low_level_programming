ea57627776d1e3b3848bc233cb458320c14c92f6
praisewairimu <misssampraise@gmail.com>
2022-09-11 02:36:02 -0700

    Lower case and a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - print if the number is positive, zero, or negative
- * Description: uing the main function
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
@@ -12,7 +13,7 @@ for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 if (ch != 'e' && ch != 'q')
 {
-        putchar(ch);
+	putchar(ch);
 }
 }
 putchar('\n');
