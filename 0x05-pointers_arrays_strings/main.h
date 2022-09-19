964889cf8347d0754945a6da7b38a7be05f740ef
praisewairimu <misssampraise@gmail.com>
2022-09-19 23:04:51 +0300

    My commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * void prototypes()
- * int prototypes()
- */
+* void prototypes()
+* int prototypes()
+*/
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
